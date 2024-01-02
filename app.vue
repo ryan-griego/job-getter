ca8ea423888c51514b9d9d582491725bbddddbca
Ryan Griego <ryangriego@gmail.com>
2024-01-02 09:31:09 -0800

    testing new repo config

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@
     >
       <template v-slot:top>
         <v-toolbar flat>
-          <v-toolbar-title>Patient Admin</v-toolbar-title>
+          <v-toolbar-title>Job Getter</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="500px">

2a4ad1ae14f1695b550f4d12fbfafe40d4055252
Ryan Griego <ryangriego@gmail.com>
2024-01-04 17:29:20 -0800

    Added tabulator with dummy data

--- app.vue
+++ app.vue
@@ -1,121 +1,18 @@
 <template>
     <v-container>
-      <v-row align="center"
-        justify="center">
-        <v-col
-          class="mb-5"
-          cols="12"
-        >
-    <v-data-table
-      :headers="headers"
-      :items="jobs"
-      :sort-by="[{ key: 'companyUrl', order: 'asc' }]"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>Job Getter</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ props }">
-              <v-btn color="primary" dark class="mb-2" v-bind="props">
-                New Job
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
+         <div>
+            <h1 class="text-center large-text">Job Getter</h1>
+        <Table @send-email="sendEmail"/>
+     </div>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.companyUrl"
-                        label="Patient companyUrl"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.calories"
-                        label="Company Name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.fat"
-                        label="Job Title"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Job Location"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.postedAt"
-                        label="Posted At"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="close">
-                  Cancel
-                </v-btn>
-                <v-btn color="blue-darken-1" variant="text" @click="save">
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="600px">
-            <v-card>
-              <v-card-title class="text-h5"
-                >Are you sure you want to delete this patient?</v-card-title
-              >
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
-                  >Cancel</v-btn
-                >
-                <v-btn
-                  color="blue-darken-1"
-                  variant="text"
-                  @click="deleteItemConfirm"
-                  >OK</v-btn
-                >
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon size="small" class="me-2" @click="editItem(item)">
-          mdi-pencil
-        </v-icon>
-        <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
-        <v-icon size="small" @click="sendEmail(item)"> mdi-mail </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize"> Reset </v-btn>
-      </template>
-    </v-data-table>
 
-     </v-col>
-    </v-row>
   </v-container>
 
  </template>
 
 <script>
-
+   import Table from './components/Table.vue';
+   import 'tabulator-tables/dist/css/tabulator_midnight.min.css';
 
   export default {
     data: () => ({
@@ -233,22 +130,26 @@
           },
         ]
       },
-      editItem(item) {
-        this.editedIndex = this.jobs.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
+      // editItem(item) {
+      //   this.editedIndex = this.jobs.indexOf(item)
+      //   this.editedItem = Object.assign({}, item)
+      //   this.dialog = true
+      // },
 
-      deleteItem(item) {
-        this.editedIndex = this.jobs.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
+      // deleteItem(item) {
+      //   this.editedIndex = this.jobs.indexOf(item)
+      //   this.editedItem = Object.assign({}, item)
+      //   this.dialogDelete = true
+      // },
 
     async sendEmail(item) {
-      let html = ""
+      console.log('log the item in sendEmail', item);
+      item = toRaw(item);
+            // console.log('log the item in senddddEmail', item);
 
-      let companyUrl =  item.companyUrl;
+      let html = ""
+      let companyUrl = 'item.companyUrl';
+      // let companyUrl =  item.companyUrl;
       let companyName = item.companyName;
       let jobTitle = item.jobTitle;
       let jobLocation = item.jobLocation;
@@ -1268,90 +1169,6 @@ Call me maybe?
         body: msg
       });
     },
-
-      deleteItemConfirm() {
-        this.jobs.splice(this.editedIndex, 1)
-        this.closeDelete()
-      },
-
-      close() {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      closeDelete() {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save() {
-        if (this.editedIndex > -1) {
-          Object.assign(this.jobs[this.editedIndex], this.editedItem)
-        } else {
-          this.jobs.push(this.editedItem)
-        }
-        this.close()
-      },
-
-
-
-
-    },
+   },
   }
-
-
-
-
 </script>
-
-
-
-
-<!--
-<template>
-  <div id="app">
-    <button @click="sendMail">Test</button>
-  </div>
-</template>
-
-<script>
-export default {
-  companyUrl: "App",
-  data() {
-    return {
-      companyNameTemplateString: "<p>Hello world!</p>",
-    };
-  },
-  methods: {
-    async sendMail() {
-      const result = await fetch("http://localhost:3000/sendmail", {
-        body: JSON.stringify(this.createCompany NameBody()),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-
-      if (result.status === 200) {
-        alert("Mail sent successfully!");
-      }
-    },
-    createCompany NameBody() {
-      return {
-        content: [
-          {
-            type: "text/html",
-            value: this.companyNameTemplateString,
-          },
-        ],
-      };
-    },
-  },
-};
-</script> -->
--- server/api/sendgrid.post.ts
+++ server/api/sendgrid.post.ts
@@ -2,6 +2,8 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const body = await readBody(event);
 
+  const key = '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';
+
   const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
     {
       method: "POST",
@@ -16,34 +18,3 @@ export default defineEventHandler(async (event) => {
     });
   return sgRequest;
 });
-
-
-
-// export default defineEventHandler(async (event) => {s
-//   const config = useRuntimeConfig();
-//   const body = await readBody(event);
-//   console.log("log the config", config);
-
-//   console.log("log the info", config.SENDGRID_API_KEY);
-// console.log("log bin", bin);
-
-
-
-
-
-//   const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
-//     {
-//       method: "POST",
-//       headers: {
-//         Authorization: "Bearer " + config.SENDGRID_API_KEY,
-//         "Content-Type": "application/json",
-//         Accept: "application/json"
-//       },
-//       body: JSON.stringify(body)
-//     }).catch((error) => {
-//       return error;
-//     });
-
-//     console.log("log the result", sgRequest);
-//   return sgRequest;
-// });

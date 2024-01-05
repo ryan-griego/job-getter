eeea6617bf485520f484bc43f0de06f893a06842
Ryan Griego <ryangriego@gmail.com>
2024-01-04 17:33:23 -0800

    added tabulator table with dummy data

--- server/api/sendgrid.post.ts
+++ server/api/sendgrid.post.ts
@@ -16,34 +16,3 @@ export default defineEventHandler(async (event) => {
     });
   return sgRequest;
 });
-
-
-
-// export default defineEventHandler(async (event) => {s
-//   const config = useRuntimeConfig();
-//   const body = await readBody(event);
-//   console.log("log the config", config);
-
-//   console.log("log the info", config.SENDGRID_API_KEY);
-// console.log("log bin", bin);
-
-
-
-
-
-//   const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
-//     {
-//       method: "POST",
-//       headers: {
-//         Authorization: "Bearer " + config.SENDGRID_API_KEY,
-//         "Content-Type": "application/json",
-//         Accept: "application/json"
-//       },
-//       body: JSON.stringify(body)
-//     }).catch((error) => {
-//       return error;
-//     });
-
-//     console.log("log the result", sgRequest);
-//   return sgRequest;
-// });

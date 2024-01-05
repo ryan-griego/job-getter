84557ebcce7ef208c34ef2d249cde466ba38015c
Ryan Griego <ryangriego@gmail.com>
2024-01-04 17:34:58 -0800

    fixed

--- server/api/sendgrid.post.ts
+++ server/api/sendgrid.post.ts
@@ -2,8 +2,6 @@ export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
   const body = await readBody(event);
 
-  const key = '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';
-
   const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
     {
       method: "POST",

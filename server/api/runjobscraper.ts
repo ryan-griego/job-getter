e7524019f8a61fed2fcb5156899ab19ff7a258db
Ryan Griego <ryangriego@gmail.com>
2024-03-18 14:46:20 -0700

    Added ability update domain, job poster name and company url

--- server/api/runjobscraper.ts
+++ server/api/runjobscraper.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event) => {
         Accept: "application/json",
         "X-Phantombuster-Key": config.PHANTOMBUSTER_API_KEY
       },
-      body: JSON.stringify({ id: '5183562289175487' })
+      body: JSON.stringify({ id: '4512793125431671' })
     }).catch((error) => {
       console.log("log the error", error);
       return error;
